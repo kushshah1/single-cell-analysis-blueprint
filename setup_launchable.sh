e12b44eb1b84fa79f6978c27c201006a6e006324
Kushal Shah <kshah1996@hotmail.com>
2026-06-10 17:58:43 -0700

    updated dirs (up a level)

--- setup_launchable.sh
+++ setup_launchable.sh
@@ -6,7 +6,7 @@ export PATH="$HOME/.local/bin:$PATH"
 uv --version
 
 # Install all additional libraries
-cd /notebooks
+cd /notebooks/workshop/single-cell-analysis-blueprint
 uv pip install --system -r requirements.txt
 mamba install -c conda-forge -c rapidsai cupy==14.0.1 numpy==2.2.6 -y
 
@@ -14,4 +14,4 @@ set -m
 
 # Set missing env vars, start the primary process, and put it in the background
 export CONDA_PREFIX=/opt/conda
-jupyter-lab --notebook-dir=/notebooks --ip=0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.allow_origin='*' --allow-root
+jupyter-lab --notebook-dir=/notebooks/workshop/single-cell-analysis-blueprint --ip=0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.allow_origin='*' --allow-root
